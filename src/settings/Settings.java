9555f6468e763b652c1c0c4f07c375a727300e3c
Ziren <zirenx@unimelb.edu.au>
2019-04-27 11:13:08 +0800

    object to bytes

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -1,8 +1,17 @@
 package settings;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 
 
 public class Settings {
@@ -29,5 +38,46 @@ public class Settings {
 		}
 		return null;
     }
+	
+	public static byte[] encodes(Object obj) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(out);
+		oos.writeObject(obj);
+		oos.flush();
+		oos.close();
+		byte[] bytes = out.toByteArray();
+		return bytes;
+	}
+ 
+	public static Object decode(byte[] bytes) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+		ObjectInputStream inn = new ObjectInputStream(in);
+		Object obj = inn.readObject();
+		return obj;
+	}
+ 
+	/**
+	 * byte to buf
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public ByteBuf getBufFromByte(byte[] bytes) {
+		ByteBuf buf = Unpooled.copiedBuffer(bytes);
+		return buf;
+	}
+ 
+	/**
+	 * buf to byte
+	 * 
+	 * @param buf
+	 * @return
+	 */
+	public byte[] getByteFromBuf(ByteBuf buf) {
+		int size = buf.readableBytes();
+		byte[] bytes = new byte[size];
+		buf.readBytes(bytes);
+		return bytes;
+	}
 
 }
